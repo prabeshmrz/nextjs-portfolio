d8c3493904970f81f615302f9988d5e2920f4a73
Prabesh Maharjan <prabeshmrz.pm@gmail.com>
2024-10-29 21:18:06 +0545

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/theme-toggle";
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <ModeToggle />
+    </div>
+  );
+}
